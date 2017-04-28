202401d009a277181aa0d2a15910c0b7f3db48be
AustinG20 <agobby471@gmail.com>
2017-04-28 11:06:47 -0400

    1

--- universaldb.sql
+++ universaldb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2017 at 04:35 PM
+-- Generation Time: Apr 28, 2017 at 05:05 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.5.24
 
@@ -31,8 +31,18 @@ CREATE TABLE IF NOT EXISTS `course` (
   `type` varchar(10) NOT NULL,
   `day` varchar(10) NOT NULL,
   `startTime` datetime NOT NULL,
-  `endTime` datetime NOT NULL
+  `endTime` datetime NOT NULL,
+  `scheduleId` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`courseId`, `type`, `day`, `startTime`, `endTime`, `scheduleId`) VALUES
+(1, 'regular', 'Monday', '2017-04-10 10:00:00', '2017-04-10 12:00:00', 1),
+(2, 'regular', 'Wednesday', '2017-04-12 14:00:00', '2017-04-12 16:00:00', 2),
+(3, 'truck', 'Friday', '2017-04-14 16:00:00', '2017-04-14 18:00:00', 3);
 
 -- --------------------------------------------------------
 
@@ -77,7 +87,21 @@ INSERT INTO `students` (`studentId`, `firstName`, `lastName`, `phoneNumber`, `em
 (7, 'An', 'lname', 6541239552, 5143287450, '3456 Birnam', '2017-12-06', 2, '2017-12-12', ''),
 (8, 'Gio', 'lname', 7881209380, 5149999678, '1230 Liege', '2017-04-18', 500, '2017-03-13', ''),
 (9, 'Luke', 'Skywalker', 5148887777, 5143322116, '7432 Alderon', '2017-08-25', 5000, '2016-10-18', ''),
-(10, 'Obiwan', 'Kanobi', 982387128, 2348761234, '0278 Mustafar', '2017-04-18', 25, '2017-07-13', 'Tamil');
+(10, 'Obiwan', 'Kanobi', 982387128, 2348761234, '0278 Mustafar', '2017-04-18', 25, '2017-07-13', 'Tamil'),
+(11, 'Darlene', 'Pratt', 1239420834, 6326871278, '1285 MyAddress', '2017-07-19', 88, '2017-12-13', ''),
+(12, 'Janet', 'Perez', 865122222, 1271118423, '599 Stuart', '2016-06-21', 122, '2017-04-16', 'French'),
+(13, 'Jeannette ', 'Lambert', 2229872344, 22245719932, '1222 Danvers', '2015-09-09', 12, '2017-11-02', 'Tamil'),
+(14, 'Tracy', 'McGrady', 5149999433, 5147149999, '4567 Querbs', '2016-06-27', 555, '2018-03-08', ''),
+(15, 'Alvin ', 'Adams', 1239876123, 5143333333, '786 Jean-Talon', '2017-02-15', 9, '2017-04-04', ''),
+(16, 'Gabriel', 'Olson', 3338763456, 3331234098, '472 Namur', '2017-04-11', 800, '2017-08-23', ''),
+(17, 'Tyler', 'Burgass', 4325678900, 4324567321, '7894 Jean-Talon', '2017-04-19', 25.5, '2017-11-10', 'French'),
+(18, 'Patty', 'Martinez', 2279876345, 1234781234, '5678 Ogilvy', '2017-11-09', 50, '2016-10-12', 'Tamil'),
+(19, 'Mathieu ', 'Boileau', 1239864247, 2467855867, '5432 Lolololol', '2017-04-30', 9000, '2017-03-13', 'French'),
+(20, 'Jeremya', 'lname', 4329876234, 2340987456, '234 Addresss', '2016-12-13', 12, '2017-04-05', 'Tamil'),
+(21, 'Roxanne', 'Caldwell', 5447657777, 4321234444, '1234 Querbs', '2016-04-19', 500, '2017-04-04', ''),
+(22, 'Willard', 'Cannon', 3456789098, 3452345677, '456 Stuart', '2017-01-09', 0, '2017-04-02', ''),
+(23, 'Adam', 'Adams', 911, 911, '123 PoliceStation', '2016-07-18', 0, '2017-04-19', 'French'),
+(24, 'Bob', 'Barker', 5149990432, 1234567890, '7651 ', '2017-04-13', 59, '2017-04-09', '');
 
 -- --------------------------------------------------------
 
@@ -105,6 +129,24 @@ INSERT INTO `users` (`userId`, `email`, `password`, `studentId`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`courseId`), ADD KEY `courseId` (`courseId`,`scheduleId`);
+
+--
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`scheduleId`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`studentId`), ADD KEY `studentId` (`studentId`);
 
 --
 -- Indexes for table `users`
